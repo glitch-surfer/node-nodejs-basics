bad2d65b314b137eb78099e7f1b1133e3876bc5f
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-09-30 23:28:25 +0400

    feat: implement fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import {fileURLToPath} from 'url';
+import fs from 'fs';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.join(__filename, '..');
+
+    const sourceFolderName = 'files';
+    const destinationFolderName = 'files_copy';
+    const sourceFolderPath = path.join(__dirname, sourceFolderName);
+    const destinationFolderPath = path.join(__dirname, destinationFolderName);
+    const isDestinationFolderExists = fs.existsSync(destinationFolderPath);
+    const isSourceFolderExists = fs.existsSync(sourceFolderPath);
+
+    if (isDestinationFolderExists || !isSourceFolderExists) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await fs.promises.cp(sourceFolderPath, destinationFolderPath, {recursive: true});
+    } catch (error) {
+        console.dir(error)
+        throw new Error('FS operation failed');
+    }
+
 };
 
 await copy();
