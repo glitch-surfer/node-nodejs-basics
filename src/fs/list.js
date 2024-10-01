c05b7c8da27a9eee0a616e864570db4b47945b80
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-01 22:25:58 +0400

    feat: implement fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import fs from "fs";
+
 const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.join(__filename, '..');
+
+    const folderName = 'files';
+    const folderPath = path.join(__dirname, folderName);
+
+    if (!fs.existsSync(folderPath)) {
+        throw new Error('FS operation failed');
+    }
+
+    const files = await fs.promises.readdir(folderPath);
+    console.log(files);
 };
 
 await list();
