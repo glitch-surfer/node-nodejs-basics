b706acc67df66a11a4834917af9536b89fe83403
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-01 22:28:59 +0400

    feat: implement fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.join(__filename, '..');
+
+    const fileName = 'fileToRead.txt'
+    const filePath = path.join(__dirname, 'files', fileName);
+
+    if (!fs.existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    const content = await fs.promises.readFile(filePath, 'utf-8');
+    console.log(content);
 };
 
 await read();
