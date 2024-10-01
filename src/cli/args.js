ce93a575d94136abe93b62a4f2cc51ecc55c34d9
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-01 22:47:16 +0400

    feat: implement cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv
+        .slice(2)
+        .reduce((acc, arg, index, arr) => {
+            if (index % 2 === 0 && index !== arr.length - 1) {
+                const formattedValue = `${arg.slice(2)} is ${arr[index + 1]}`;
+                acc.push(formattedValue)
+            }
+            return acc
+        }, [])
+        .join(', ');
+
+    console.log(args)
 };
 
 parseArgs();
