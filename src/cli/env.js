89667f86d47647d5fe83429a5c2efbb103cd8c86
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-01 22:35:54 +0400

    feat: implement cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envs = process.env;
+    const rssEnvs = Object.entries(envs)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    console.log(rssEnvs)
 };
 
 parseEnv();
