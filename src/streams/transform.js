8df5138b9344c67eef76c33b1dec0f88ad3722c0
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-02 23:29:04 +0400

    feat: implement stream:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import {pipeline, Transform} from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            try {
+                const transformedChunk = chunk.toString().split('').reverse().join('')
+                this.push(transformedChunk);
+                callback();
+            } catch (err) {
+                callback(err);
+            }
+        }
+    })
+
+    pipeline(
+        process.stdin,
+        transformStream,
+        process.stdout,
+        (err) => {
+            if (err) {
+                console.error(err);
+            }
+        }
+    )
 };
 
 await transform();
