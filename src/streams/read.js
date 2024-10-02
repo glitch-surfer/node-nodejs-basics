5486d6e90833e13ce0e0f4e984c0631ff146ac7c
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-02 23:10:04 +0400

    feat: implement stream:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import fs from "fs";
+import {pipeline} from "stream";
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.join(__filename, '..');
+
+    const fileName = 'fileToRead.txt'
+    const filePath = path.join(__dirname, 'files', fileName);
+
+    if (!fs.existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    fs.createReadStream(filePath).pipe((process.stdout));
 };
 
 await read();
