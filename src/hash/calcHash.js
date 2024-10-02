5f389a07eb63b717dd4f67ae7110653bd46a2c84
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-02 23:03:17 +0400

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import fs from "fs";
+import {pipeline} from 'stream';
+import {createHash} from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.join(__filename, '..');
+
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const readFileStream = fs.createReadStream(filePath);
+    const hashTransformStream = createHash('sha256');
+
+    pipeline(
+        readFileStream,
+        hashTransformStream,
+        (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(hashTransformStream.digest('hex'));
+            }
+        }
+    )
 };
 
 await calculateHash();
