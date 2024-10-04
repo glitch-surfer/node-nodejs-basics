cff0dc7e87304e9f1e827c8ec2c3bb360f983072
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-04 19:36:21 +0400

    feat: implement zip:decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,33 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import fs from "fs";
+import {pipeline} from "stream";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.join(__filename, '..');
+
+    const zipName = 'archive.gz'
+    const zipFilePath = path.join(__dirname, 'files', zipName);
+
+    const fileName = 'fileToCompress.txt'
+    const filePath = path.join(__dirname, 'files', fileName);
+
+    if (!fs.existsSync(zipFilePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    pipeline(
+        fs.createReadStream(zipFilePath),
+        zlib.createUnzip(),
+        fs.createWriteStream(filePath),
+        (err) => {
+            if (err) {
+                console.error(err);
+            }
+        }
+    )
 };
 
 await decompress();
