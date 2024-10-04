b5b256a46278bb0694c654c4137c24885d59f71b
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-04 19:32:21 +0400

    feat: implement zip:compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,33 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import fs from "fs";
+import {pipeline} from "stream";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.join(__filename, '..');
+
+    const fileName = 'fileToCompress.txt'
+    const filePath = path.join(__dirname, 'files', fileName);
+
+    const zipName = 'archive.gz'
+    const zipFilePath = path.join(__dirname, 'files', zipName);
+
+    if (!fs.existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    pipeline(
+        fs.createReadStream(filePath),
+        zlib.createGzip(),
+        fs.createWriteStream(zipFilePath),
+        (err) => {
+            if (err) {
+                console.error(err);
+            }
+        }
+    )
 };
 
 await compress();
